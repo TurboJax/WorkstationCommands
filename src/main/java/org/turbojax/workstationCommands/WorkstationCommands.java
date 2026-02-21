fb352d29736880b78888a9f7ac2808820d11281e
TurboJax <turbojax07@gmail.com>
2026-02-20 22:21:37 -0500

    Removing permission configuration and renaming default commands

--- src/main/java/org/turbojax/workstationCommands/WorkstationCommands.java
+++ src/main/java/org/turbojax/workstationCommands/WorkstationCommands.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import org.bukkit.Bukkit;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.MenuType;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -18,38 +20,29 @@ public final class WorkstationCommands extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
 
-        HashMap<String,MenuType> menuTypes = new HashMap<>();
-        menuTypes.put("craftingtable", MenuType.CRAFTING);
-        menuTypes.put("stonecutter", MenuType.STONECUTTER);
-        menuTypes.put("loom", MenuType.LOOM);
-        menuTypes.put("cartographytable", MenuType.CARTOGRAPHY_TABLE);
-        menuTypes.put("furnace", MenuType.FURNACE);
-        menuTypes.put("blastfurnace", MenuType.BLAST_FURNACE);
-        menuTypes.put("smoker", MenuType.SMOKER);
-        menuTypes.put("smithingtable", MenuType.SMITHING);
-        menuTypes.put("anvil", MenuType.ANVIL);
-        menuTypes.put("enchantingtable", MenuType.ENCHANTMENT);
-        menuTypes.put("grindstone", MenuType.GRINDSTONE);
-        menuTypes.put("brewingstand", MenuType.BREWING_STAND);
+        HashMap<String, MenuType> menuTypes = new HashMap<>();
+        menuTypes.put("wccraft", MenuType.CRAFTING);
+        menuTypes.put("wcstonecutter", MenuType.STONECUTTER);
+        menuTypes.put("wcloom", MenuType.LOOM);
+        menuTypes.put("wccartography", MenuType.CARTOGRAPHY_TABLE);
+        menuTypes.put("wcfurnace", MenuType.FURNACE);
+        menuTypes.put("wcblastfurnace", MenuType.BLAST_FURNACE);
+        menuTypes.put("wcsmoker", MenuType.SMOKER);
+        menuTypes.put("wcsmithing", MenuType.SMITHING);
+        menuTypes.put("wcanvil", MenuType.ANVIL);
+        menuTypes.put("wcenchant", MenuType.ENCHANTMENT);
+        menuTypes.put("wcgrindstone", MenuType.GRINDSTONE);
+        menuTypes.put("wcbrew", MenuType.BREWING_STAND);
 
         // Loading commands if enabled in the config
         menuTypes.forEach((label, menuType) -> {
             if (getConfig().getBoolean(label + ".enabled")) {
-                // Registering permissions
-                Permission permission = new Permission(getConfig().getString(label + ".permission.name"));
-                permission.setDefault(PermissionDefault.getByName(getConfig().getString(label + ".permission.default-level")));
-                permission.setDescription(getConfig().getString(label + ".permission.description"));
-                getServer().getPluginManager().addPermission(permission);
+                String basePermission = "wc." + label.substring(2);
 
-                Permission otherPermission = new Permission(getConfig().getString(label + ".other-permission.name"));
-                otherPermission.setDefault(PermissionDefault.getByName(getConfig().getString(label + ".other-permission.default-level")));
-                otherPermission.setDescription(getConfig().getString(label + ".other-permission.description"));
-                getServer().getPluginManager().addPermission(otherPermission);
-                
                 // Getting the command
                 PluginCommand command = getCommand(label);
                 assert command != null;
-                command.setPermission(getConfig().getString(label + ".permission.name"));
+                command.setPermission(basePermission);
 
                 // Assigning the Executor and TabCompleter
                 command.setExecutor(new CommandExecutor() {
@@ -58,15 +51,14 @@ public final class WorkstationCommands extends JavaPlugin {
                         // Handling when the command is given a parameter
                         if (args.length == 1) {
                             // Verifying that the sender has the right permissions
-
-                            if (!sender.hasPermission(getConfig().getString(label + ".other-permission.name"))) {
-                                sender.sendMessage("You don't have permission to run this command!");
+                            if (!sender.hasPermission(basePermission + ".other")) {
+                                sender.sendMessage("§cYou don't have permission to run this command!");
                                 return false;
                             }
 
                             Player player = Bukkit.getPlayer(args[0]);
                             if (player == null || !player.isOnline()) {
-                                sender.sendMessage("Could not find player \"" + args[0] + "\" online.");
+                                sender.sendMessage("§cCould not find player \"" + args[0] + "\" online.");
                                 return false;
                             }
 
@@ -86,21 +78,20 @@ public final class WorkstationCommands extends JavaPlugin {
                     }
                 });
                 command.setTabCompleter(new TabCompleter() {
-                            @Override
-                            public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-                                if (sender.hasPermission(getConfig().getString(label + ".other-permission.name"))) {
-                                    List<String> players = new ArrayList<String>();
-                                    for (Player player : Bukkit.getOnlinePlayers()) {
-                                        players.add(player.getName());
-                                    }
-
-                                    return players;
-                                }
-
-                                return List.of();
+                    @Override
+                    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+                        if (sender.hasPermission(basePermission + ".other")) {
+                            List<String> players = new ArrayList<String>();
+                            for (Player player : Bukkit.getOnlinePlayers()) {
+                                players.add(player.getName());
                             }
+
+                            return players;
                         }
-                );
+
+                        return List.of();
+                    }
+                });
 
                 // Registering aliases
                 for (String alias : getConfig().getStringList(label + ".aliases")) {
